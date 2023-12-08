1707d5576476bde7c104dd4249ff5dc827f70d97
CJH <chenjianhuisiren@163.com>
2023-12-08 16:06:48 +0800

    添加了vulkanmodule静态类库，opengl 和vulkan

---
+++ VulkanModule/src/CMaketest.cpp
@@ -0,0 +1,30 @@
+﻿// CMaketest.cpp: 定义应用程序的入口点。
+//
+
+#include "VulkanModule/CMaketest.h"
+
+using namespace std;
+
+//int main()
+//{
+//	cout << "Hello CMake." << endl;
+//	return 0;
+//}
+
+namespace VulkanModule
+{
+	myTest::myTest()
+	{
+
+	}
+
+	myTest::~myTest()
+	{
+
+	}
+
+	void myTest::testCount()
+	{
+		cout << "Hello VulkanModule." << endl;
+	}
+}
