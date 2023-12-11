288777e5803ea0609f9dbf5a41cc20d24ea763fa
CJH <chenjianhuisiren@163.com>
2023-12-11 17:01:48 +0800

    vulkan 窗口仍未解决

--- VulkanTest/VulkanMain.cpp
+++ VulkanTest/VulkanMain.cpp
@@ -27,27 +27,27 @@ using namespace std;
 
 int main() {
     cout << "Hello CMake Vulkan demo." << endl;
-    //glfwInit();
+    glfwInit();
 
-    //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    //GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    //uint32_t extensionCount = 0;
-    //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    //std::cout << extensionCount << " extensions supported" << std::endl;
+    std::cout << extensionCount << " extensions supported" << std::endl;
 
-    //glm::mat4 matrix;
-    //glm::vec4 vec;
-    //auto test = matrix * vec;
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
 
-    //while (!glfwWindowShouldClose(window)) {
-    //    glfwPollEvents();
-    //}
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
 
-    //glfwDestroyWindow(window);
+    glfwDestroyWindow(window);
 
-    //glfwTerminate();
+    glfwTerminate();
 
     return 0;
 }
